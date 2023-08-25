0add4a42f2d2f1e4a54abc71ec87aa272efa138f
shivakumar <shivakumarhosamani02@gmail.com>
2023-08-25 15:48:26 +0530

    addition of two number

---
+++ add.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "c68a641a",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "main.py\n",
+    "def add(a, b):\n",
+    " return a + b\n",
+    "result = add(5, 3)\n",
+    "print(\"Result:\", result)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a5dbe80c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
